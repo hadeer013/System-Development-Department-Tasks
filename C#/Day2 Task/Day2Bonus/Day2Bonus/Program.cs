53fe9b82c1de06708bf15b9d40c431a3423126b0
HadeerRamzy <hadeerramzy1000@gmail.com>
2024-03-02 21:13:41 +0200

    Day 2 Task

---
+++ C#/Day2 Task/Day2Bonus/Day2Bonus/Program.cs
@@ -0,0 +1,94 @@
+﻿namespace Day2Bonus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float budget = 183.23f;
+            float bagVolume = 64.11f;
+            int people = 7;
+            int Npresents = 12;
+            float[] presentVolume = { 4.53f, 9.11f, 4.53f, 6.00f, 1.04f, 0.87f, 2.57f, 19.45f, 65.59f, 14.14f, 16.66f, 13.53f };
+            float[] presentPrice  = { 12.23f, 45.03f, 12.23f, 32.93f, 6.99f, 0.46f, 7.34f, 65.98f, 152.13f, 7.23f, 10.00f, 25.25f };
+
+            float Result = PresentList(budget, bagVolume, people, Npresents, presentVolume, presentPrice);
+            
+            Console.WriteLine($"Maximum price : {Result}");
+
+
+            //Console.WriteLine(BackTrack(budget, bagVolume, people, Npresents, 0, presentVolume, presentPrice)); 
+        }
+
+
+        public static float PresentList(float budget, float bagVolume, int people, int
+            Npresents, float[] presentVolume, float[] presentPrice)
+        {
+            float[,] dp = new float[Npresents + 1, (int)Math.Ceiling(bagVolume) + 1];
+            int[,] NumberOfPresentsTaken = new int[Npresents + 1, (int)Math.Ceiling(bagVolume) + 1];
+            for (int i = 0; i < dp.GetLength(0); i++)
+            {
+                for (int j = 0; j < dp.GetLength(1); j++)
+                {
+                    if (i == 0 || j == 0) { dp[i, j] = 0; NumberOfPresentsTaken[i, j] = 0; }
+
+                    else if (presentVolume[i - 1] <= j)
+                    {
+                        dp[i, j] = Math.Max(dp[i - 1, j], presentPrice[i - 1] + dp[i - 1, j - (int)Math.Ceiling(presentVolume[i - 1])]);
+                        if (dp[i, j] > presentPrice[i - 1])
+                            NumberOfPresentsTaken[i, j] = 1 + NumberOfPresentsTaken[i - 1, j - (int)Math.Ceiling(presentVolume[i - 1])];
+                    }
+
+                    else
+                    {
+                        dp[i, j] = dp[i - 1, j];
+                        NumberOfPresentsTaken[i, j] = NumberOfPresentsTaken[i - 1, j];
+                    }
+
+                }
+            }
+            float maxPrice = 0.0f;
+            int presentNum = 0;
+            for (int i = 1; i < dp.GetLength(0); i++)
+            {
+                for (int j = 1; j < dp.GetLength(1); j++)
+                {
+                    if (dp[i, j] <= budget && NumberOfPresentsTaken[i, j] % people == 0 && NumberOfPresentsTaken[i, j] != 0)
+                    {
+
+                        maxPrice = Math.Max(maxPrice, dp[i, j]);
+                        presentNum = NumberOfPresentsTaken[i, j];
+                        //Console.WriteLine($"i={i} , j={j} -> maxPrice= {maxPrice} , presentNum={presentNum}");
+                    }
+                }
+            }
+
+
+            Console.WriteLine($"Number of Presents : {presentNum}");
+            return maxPrice;
+        }
+
+        #region BackTrack
+        //static float Mainbudget = 183.23f;
+        //static float BackTrack(float budget, float bagVolume, int people, int
+        //Npresents, int index, float[] presentVolume, float[] presentPrice)
+        //{
+        //    if (budget == 0 || bagVolume == 0 || index == Npresents)
+        //    {
+        //        if (index % people == 0)
+        //            return Mainbudget - budget;
+        //        return 0;
+        //    }
+        //    if (presentVolume[index]<=bagVolume && presentPrice[index]<=budget)
+        //    {
+        //        float withPresent = BackTrack(budget - presentPrice[index], bagVolume - presentVolume[index], people, Npresents, index + 1, presentVolume, presentPrice);
+        //        float withoutPresent= BackTrack(budget, bagVolume, people, Npresents, index + 1, presentVolume, presentPrice);
+        //        return Math.Max(withPresent, withoutPresent);
+        //    }
+        //    return BackTrack(budget, bagVolume, people, Npresents, index + 1, presentVolume, presentPrice);
+        //}
+        #endregion
+
+    }
+}
+
---
+++ C#/Day2 Task/c#Day2Task/c#Day2Task/Program.cs
@@ -0,0 +1,161 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace c_Day2Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region LongestDistance
+            Console.WriteLine("Enter size of array");
+            int size = int.Parse(Console.ReadLine());
+
+            int[] arrNum = new int[size];
+
+            for (int i = 0; i < size; i++)
+                arrNum[i] = int.Parse(Console.ReadLine());
+
+            int maxDis = LongestDistance(arrNum);
+            Console.WriteLine(maxDis);
+
+            #endregion
+
+            #region Reverse Sentence
+            //ReverseWords("my name is hadeer");
+            #endregion
+
+            #region CountOneOccurances
+
+            //Stopwatch stopwatch = new Stopwatch();
+
+            //stopwatch.Start();
+            //Console.WriteLine($"Number of ones : {CountOneOccurancesUsingSplit(1, 99_999_999)}");
+            //stopwatch.Stop();
+
+            //Console.WriteLine($"Time elapsed in millisecond using Split: {stopwatch.Elapsed}");
+            //Console.WriteLine("=====================================================");
+
+            //stopwatch.Restart();
+            //Console.WriteLine($"Number of ones : {CountOneOccurancesLoopsOverStr(1, 99_999_999)}");
+            //stopwatch.Stop();
+
+            //Console.WriteLine($"Time elapsed in millisecond using LoopsOverStr: {stopwatch.Elapsed}");
+            //Console.WriteLine("=====================================================");
+
+
+
+            //stopwatch.Restart();
+            //Console.WriteLine($"Number of ones : {CountOnesOccurancesMathematically(1, 99_999_999)}");
+            //stopwatch.Stop();
+
+            //Console.WriteLine($"Time elapsed in millisecond using integers: {stopwatch.Elapsed}");
+            //Console.WriteLine("=====================================================");
+
+            //stopwatch.Restart();
+            //Console.WriteLine($"Number of ones : {CountOnesOccurancesMathematicEquation(8)}");
+            //stopwatch.Stop();
+
+            //Console.WriteLine($"Time elapsed in millisecond using Equation: {stopwatch.Elapsed}");
+            //Console.WriteLine("=====================================================");
+            #endregion
+        }
+
+
+        static int LongestDistance(int[] arr)
+        {
+            int maxDis = -1;
+
+            for(int i = 0; i < arr.Length-1; i++)
+                for(int j = i+1;j<arr.Length;j++)
+                {
+                    if (arr[i] == arr[j])
+                        maxDis = Math.Max(maxDis, j - i-1);
+
+                }
+            return maxDis;
+        }
+
+        static void ReverseWords(string sentence)
+        {
+            string[]splitedWords = sentence.Split(' ');
+            Array.Reverse(splitedWords);
+
+            StringBuilder Reversed=new StringBuilder();
+            for (int i = 0; i < splitedWords.Length; i++)
+            {
+                Reversed.Append(splitedWords[i] );
+                if(i!= splitedWords.Length-1)
+                    Reversed.Append(' ');
+            }
+
+            Console.WriteLine(Reversed);
+        }
+
+        static int CountOneOccurancesUsingSplit(int startNumber, int endNumber)
+        {
+            int count = 0;
+            for (int i = startNumber; i <= endNumber; i++)
+            {
+                string value = Convert.ToString(i);
+                count += (value.Split('1').Length-1);
+
+            }
+            return count;
+        }
+
+        #region IndexOfTest
+        //static int CountOneOccurancesUsingIndexOf(int startNumber, int endNumber)
+        //{
+        //    int count = 0,st_index=0;
+        //    for (int i = startNumber; i <= endNumber; i++)
+        //    {
+        //        string value = Convert.ToString(i);
+        //        do
+        //        {
+        //            st_index = value.IndexOf('1', st_index);
+        //            if (st_index != -1) { count++; st_index++; }
+        //        } while (st_index!=-1 || st_index>value.Length);
+
+        //    }
+        //    return count;
+        //}
+        #endregion
+        static int CountOneOccurancesLoopsOverStr(int startNumber, int endNumber)
+        {
+            int count = 0;
+            for (int i = startNumber; i < endNumber; i++)
+            {
+                string value = Convert.ToString(i);
+                for (int j = 0; j < value.Length; j++)
+                    if (value[j] == '1') count++;
+            }
+            return count;
+        }
+
+        static int CountOnesOccurancesMathematically(int startNumber, int endNumber)
+        {
+            int count = 0;
+            for(int i = startNumber;i <= endNumber;i++)
+            {
+                int temp = i;
+                while(temp > 0)
+                {
+                    int mod = temp % 10;
+                    if(mod == 1) count++;
+                    temp /= 10;
+                }
+            }
+            return count;
+        }
+
+        static double CountOnesOccurancesMathematicEquation(int NumOfDigits)
+        {
+            double count = NumOfDigits * Math.Pow(10, NumOfDigits - 1);
+
+            return count;
+        }
+    }
+
+    
+}
